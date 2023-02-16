0be0528b746004e24ed1098b87f8d03b90b1617a
christian <christian.crespoo.98@gmail.com>
2023-02-16 18:40:48 +0000

    add git aliases

--- home-manager/git/default.nix
+++ home-manager/git/default.nix
@@ -6,6 +6,11 @@
     userName = "christian";
     userEmail = "christian.crespoo.98@gmail.com";
 
+    aliases = {
+      pub = "push -u origin HEAD";
+      cm = "commit";
+    };
+
     extraConfig = {
       init.defaultBranch = "main";
     };
