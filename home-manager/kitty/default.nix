3444fab55b5d4f90c77cbdfec0dd80f4fc7bd4ce
christian <christian.crespoo.98@gmail.com>
2023-05-22 15:14:47 +0000

    update kitty `scrollback_pager` and borders

--- home-manager/kitty/default.nix
+++ home-manager/kitty/default.nix
@@ -7,9 +7,11 @@
     settings = { 
       font_family = "DejaVuSansMono Nerd Font Mono";
       font_size = 10;
-      window_margin_width = 10;
-      adjust_line_height = 2;
-      scrollback_pager = "nvim -c 'set nonumber ft=man nolist showtabline=0 foldcolumn=0' -c 'autocmd TermOpen * normal G' -c 'silent write /tmp/kitty_scrollback_buffer | te cat /tmp/kitty_scrollback_buffer -'";
+      draw_minimal_borders = true;
+      window_border_width = 0;
+      window_padding_width = 14;
+      adjust_line_height = 3;
+      scrollback_pager = "nvim --noplugin -c \"silent write! /tmp/kitty_scrollback_buffer | te cat /tmp/kitty_scrollback_buffer - \"";
     };
   };
 }
