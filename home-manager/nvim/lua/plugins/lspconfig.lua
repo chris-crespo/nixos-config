5c16e038454594367d587b302fea7b6b5161d42e
christian <christian.crespoo98@gmail.com>
2023-06-25 09:06:59 +0000

    add more lsp keymaps

--- home-manager/nvim/lua/plugins/lspconfig.lua
+++ home-manager/nvim/lua/plugins/lspconfig.lua
@@ -1,8 +1,11 @@
 local function on_attach(client, buffer)
   local opts = { buffer = buffer }
-  vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, opts)
+  vim.keymap.set('n', 'ca', vim.lsp.buf.code_action, opts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', 'F', vim.lsp.buf.format, opts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
   vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
