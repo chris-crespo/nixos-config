e33345b14bde83930d2ceee96bdfca4532a3d164
Christian Crespo <christian.crespoo.98@gmail.com>
2023-06-27 15:15:44 +0000

    add `jdtls`

--- home-manager/nvim/default.nix
+++ home-manager/nvim/default.nix
@@ -30,6 +30,7 @@ in {
           astro 
           css
           fish
+          java
           nix 
           lua 
           rust 
@@ -43,6 +44,8 @@ in {
       rust-tools-nvim
       telescope-nvim
       yuck-vim
+
+      nvim-jdtls
     ];
 
     extraPackages = with pkgs; [
@@ -59,4 +62,28 @@ in {
       EOF
     '';
   };
+
+  home.file.".config/nvim/ftplugin/java.lua".text = ''
+    local function on_attach(client, buffer)
+      local opts = { buffer = buffer }
+      vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, opts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+      vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+      vim.keymap.set('n', 'F', vim.lsp.buf.format, opts)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+      vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    end
+
+    local cmd = os.getenv('JDTLS_PATH')
+    local config = {
+      on_attach = on_attach,
+      cmd = { cmd, '-data', '/home/nix/.cache/jdtls/workspace/'},
+      root_dir = vim.fs.dirname(vim.fs.find({'.git', 'mvnw'}, { upward = true })[1]),
+    }
+
+    require('jdtls').start_or_attach(config)
+  '';
 }
