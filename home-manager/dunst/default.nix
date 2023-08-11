eaec3f68f84d2c54535e858c9cfb7d6ef67efcb7
Christian Crespo <christian.crespoo.98@gmail.com>
2023-08-11 15:38:11 +0000

    style dunst

--- home-manager/dunst/default.nix
+++ home-manager/dunst/default.nix
@@ -4,10 +4,31 @@
 
     settings = {
       global = {
-        width = 300;
-        height = 200;
-        offset = "20x50";
+        padding = 12;
+        horizontal_padding = 16;
+        offset = "30x12";
         transparency = 10;
+        corner_radius = 12;
+        shrink = true;
+
+        font = "DejaVuSansM Nerd Font Mono 9";
+        line_height = 0;
+
+        frame_width = 2;
+        background = "#1e1e2e";
+        foreground = "#cdd6f4";
+      };
+
+      urgency_low = {
+        frame_color = "#45475a";
+      };
+
+      urgency_normal = {
+        frame_color = "#89b4fa";
+      };
+
+      urgency_critical = {
+        frame_color = "#f38ba8";
       };
     };
   };
