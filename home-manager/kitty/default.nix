85e97883e797aa7612ed42054295afc75d2149a2
Christian Crespo <christian.crespoo.98@gmail.com>
2023-12-05 21:17:10 +0100

    kitty: add `bold` and `italic` fonts

--- home-manager/kitty/default.nix
+++ home-manager/kitty/default.nix
@@ -6,13 +6,20 @@
 
     settings = { 
       font_family = "DejaVuSansM Nerd Font Mono";
+      bold_font = "DejaVuSansM Nerd Font Mono:style=Bold";
+      italic_font = "DejaVuSansM Nerd Font Mono style=Oblique";
+
       font_size = 10;
+      adjust_line_height = 3;
       text_composition_strategy = "legacy";
+
       draw_minimal_borders = true;
       window_border_width = 0;
       window_padding_width = 14;
-      adjust_line_height = 3;
+
       scrollback_pager = "nvim --noplugin -c \"silent write! /tmp/kitty_scrollback_buffer | te cat /tmp/kitty_scrollback_buffer - \"";
     };
+
+    keybindings = { };
   };
 }
