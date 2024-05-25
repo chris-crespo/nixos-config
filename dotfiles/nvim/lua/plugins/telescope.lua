dfc5f4e4e4759252e70cd060e5a59fe869b45f83
Christian Crespo <christian.crespoo.98@gmail.com>
2024-05-25 12:05:38 +0200

    feat(nvim): add keymap to toggle `telescope` preview

--- dotfiles/nvim/lua/plugins/telescope.lua
+++ dotfiles/nvim/lua/plugins/telescope.lua
@@ -1,1 +1,12 @@
-require'telescope'.setup { }
+require 'telescope'.setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-p>'] = require 'telescope.actions.layout'.toggle_preview
+      }
+    },
+    preview = {
+      hide_on_startup = true
+    }
+  }
+}
