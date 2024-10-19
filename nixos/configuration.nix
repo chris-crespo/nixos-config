6576938d987acdecad09a28995bfd11780a6aba1
Christian Crespo <christian.crespoo.98@gmail.com>
2024-10-19 09:31:46 +0200

    chore(nixos): remove deprecated `sound.enable`

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -180,7 +180,6 @@
   # services.printing.enable = true;
 
   # Enable sound.
-  sound.enable = true;
   hardware.pulseaudio.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
